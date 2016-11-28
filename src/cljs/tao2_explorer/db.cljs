e73051926ec335ebf4f6951afd6872a2efbbf80c
chenshunli <chenshunli@ulaiber.com>
2016-11-28 15:20:39 -0800

    update data

--- src/cljs/tao2_explorer/db.cljs
+++ src/cljs/tao2_explorer/db.cljs
@@ -1,12 +1,10 @@
 (ns tao2-explorer.db)
 
-
-(def init-tableconfig {
-                       :data        [
-                                     ["" "Kia" "Nissan" "Toyota" "Honda"]
-                                     ["2008" 0 0 0 0]
-                                     ["2009" 0 0 0 0]
-                                     ["2010" 0 0 0 0]]
+(def init-tableconfig { :data [["" "Kia" "Nissan" "Toyota" "Honda"]
+                               ["2008" 321 32 345 352]
+                               ["2009" 432 233 11 321]
+                               ["2010" 1223 321 55 325]
+                               ]
                        :rowHeaders  false
                        :colHeaders  false
                        :contextMenu false})
